89be5f1caa9c4968a8d2219a04170c656740bddd
LeoChaeeeun <cheun0327@gmail.com>
2020-05-24 02:42:46 +0900

    passport init

--- nodeServerVimeo/app.js
+++ nodeServerVimeo/app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 var router = require('./router/index')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash')
 
 
 app.listen(3000, function() {
@@ -13,6 +17,15 @@ app.use(express.static('public'));
 app.use(bodyParser.json())  //bodypoarser쓴다. json형태의 데이터 응답
 app.use(bodyParser.urlencoded({extended:true}));  //json이외의 응답
 app.set('view engine','ejs') //view engine은 ejs를 쓴다고 명시
+
+app.use(session({
+	secret: 'keyboard cat',
+	resave: false,
+	saveUninitialized: true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 
 //새로운 router정의
 app.use(router)	//path없을 때 router모듈쓸래!
--- nodeServerVimeo/router/join/index.js
+++ nodeServerVimeo/router/join/index.js
@@ -3,6 +3,8 @@ var app = express()
 var router = express.Router();  //router method 실행
 var path = require('path'); //상대경로
 var mysql = require('mysql')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
 
 //DATABASE SETTING
 const connection = mysql.createConnection({
@@ -19,22 +21,31 @@ connection.connect();
 //router!
 router.get('/', function(req, res){
     console.log('get join url')
-	res.sendFile(path.join(__dirname, '../../public/join.html'))
+	res.render('join.ejs');
 })
 
-router.post('/', function(req, res){
-    var body = req.body;
-    var email = body.email;
-    var name = body.name;
-    var passwd = body.password;
+passport.use('local-join', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback : true
+    },function(req, email, password, done){
+        console.log('local-join callback called');
+    }
+));
 
-    var sql = {email : email, name : name, pw : passwd};
-
-    var query = connection.query('insert into user set ?', sql, function(err, rows) {
-        if(err){throw err;}
-        else res.render('welcome.ejs', {'name': name, 'id': rows.insertId})
-    })
-})
+//router.post('/', function(req, res){
+//    var body = req.body;
+//    var email = body.email;
+//    var name = body.name;
+//    var passwd = body.password;
+//
+//    var sql = {email : email, name : name, pw : passwd};
+//
+//    var query = connection.query('insert into user set ?', sql, function(err, rows) {
+//        if(err){throw err;}
+//        else res.render('welcome.ejs', {'name': name, 'id': rows.insertId})
+//    })
+//})
 
 
 module.exports = router;
