ac502b4ac5496d17a4211999748bbff609c35f71
LeoChaeeeun <cheun0327@gmail.com>
2020-05-26 21:05:50 +0900

    login

--- nodeServerVimeo/router/login/index.js
+++ nodeServerVimeo/router/login/index.js
@@ -20,7 +20,7 @@ connection.connect();
 
 //router!
 router.get('/', function(req, res){
-    var meg;
+    var msg;
     var errMsg = req.flash('error') //에러 메시지는 flash로 넘어온다
     if(errMsg) msg = errMsg;
 	res.render('login.ejs', {'message': msg});
@@ -47,25 +47,32 @@ passport.use('local-login', new LocalStrategy({
         var query = connection.query('select * from user where email=?',[email],function(err,rows) {
             if(err) return done(err);   //error있으면 err있다고 보냄
             if(rows.length){            //이미 email이 DB에 있으면 중복 방지
-                console.log('exsisted user')
-                return done(null, false, {message: 'your email is alread used'})
+                return done(null, {'email': email, 'id': rows[0].id})
             }else{
-                var sql = {email:email, pw:password};
-                var query = connection.query('insert into user set ?', sql, function(err, rows){
-                    if(err) throw err
-                    return done(null, {'email': email, 'id': rows.insertId})
-                })
+                    return done(null, false, {'message': 'your login info is not found'})
             }
         })
     }
 ));
 
 //이 콜백은 리다이렉트 기반으로 하는거라 json응답을 주어야한다. 
-router.post('/',passport.authenticate('local-join', {
-    //원래는 callback함수 자리지만, object literal로 표현해도 콜백함수처럼 동작
-    successRedirect: '/main',   //성공
-    failureRedirect: '/join',   //사용자 이미 있다
-    failureFlash: true })
-)
+// router.post('/',passport.authenticate('local-join', {
+//     //원래는 callback함수 자리지만, object literal로 표현해도 콜백함수처럼 동작
+//     successRedirect: '/main',   //성공
+//     failureRedirect: '/join',   //사용자 이미 있다
+//     failureFlash: true })
+// )
+
+router.post('/', function(req, res, next) {
+    passport.authenticate('local-login', function(err, user, info) {
+      if (err) res.status(500).json(err);
+      if (!user) return res.status(401).json(info.message);
+      //정상적인 경우 req.logIn하면 session저장하고 잘한다.
+      req.logIn(user, function(err) {
+        if (err) { return next(err); }
+        return res.json(user);
+      });
+    })(req, res, next);
+  })
 
 module.exports = router;
